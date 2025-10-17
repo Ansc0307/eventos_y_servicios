bb1e56219f6fc4077c90b8f6e9a7ccd651b036a7
inaDAGod <danialee14@gmail.com>
2025-10-17 19:22:58 -0400

    done reservas ms-reservas

--- ms-reservas/src/main/java/com/eventos/ms_reservas/repository/ReservaRepository.java
+++ ms-reservas/src/main/java/com/eventos/ms_reservas/repository/ReservaRepository.java
@@ -19,10 +19,6 @@ public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
     @Query("SELECT r FROM Reserva r WHERE r.fechaReservaInicio BETWEEN :inicio AND :fin")
     List<Reserva> findByFechaReservaInicioBetween(@Param("inicio") LocalDateTime inicio, @Param("fin") LocalDateTime fin);
     
-    @Query("SELECT r FROM Reserva r WHERE " +
-           "(r.fechaReservaInicio <= :fin AND r.fechaReservaFin >= :inicio)")
-    List<Reserva> findReservasConflictivas(@Param("inicio") LocalDateTime inicio, @Param("fin") LocalDateTime fin);
-
     // Native SQL equivalent (uses table/column names as in the Reserva entity)
     @Query(value = "SELECT * FROM reservas r WHERE (r.fecha_reserva_inicio <= :fin AND r.fecha_reserva_fin >= :inicio)", nativeQuery = true)
     List<Reserva> findReservasConflictivasNative(@Param("inicio") LocalDateTime inicio, @Param("fin") LocalDateTime fin);
