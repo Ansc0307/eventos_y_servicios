646cd4b3cc338d0c247d5c871b156b7d05855199
Gaby B. <gabtbm2104@gmail.com>
2025-10-18 19:57:41 -0400

    [ADD] build-all.bat

--- ms-edge-server/src/main/java/com/eventos/EdgeServerApplication.java
+++ ms-edge-server/src/main/java/com/eventos/EdgeServerApplication.java
@@ -2,10 +2,10 @@ package com.eventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 //@EnableDiscoveryClient
 public class EdgeServerApplication {
     public static void main(String[] args) {
