7427df5eb104d7c26b383ba03917be512f63b9ab
Gaby B. <gabtbm2104@gmail.com>
2025-10-18 20:20:31 -0400

    [CHECKPOINT] edge server

--- ms-edge-server/src/main/java/com/eventos/EdgeServerApplication.java
+++ ms-edge-server/src/main/java/com/eventos/EdgeServerApplication.java
@@ -2,11 +2,10 @@ package com.eventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class EdgeServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EdgeServerApplication.class, args);
