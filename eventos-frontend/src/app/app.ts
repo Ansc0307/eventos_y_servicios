8257a48625402c5c90af8c1a7d28b388f4a645ba
inaDAGod <danialee14@gmail.com>
2025-11-25 19:21:34 -0400

    solicitudes frontend working

--- eventos-frontend/src/app/app.ts
+++ eventos-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReservasListComponent } from './reservas/reservas-list.component';
+import { SolicitudesListComponent } from './solicitudes/solicitudes-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReservasListComponent],
+  imports: [RouterOutlet, ReservasListComponent, SolicitudesListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
