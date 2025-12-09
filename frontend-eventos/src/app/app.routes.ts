65352dfaebd3120e2ced46806a856134989d6ff1
Andy Laffertt <alaffertt@gmail.com>
2025-12-09 11:43:34 -0400

    jalando main y comentando carpeta borrador

--- frontend-eventos/src/app/app.routes.ts
+++ frontend-eventos/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { OfertasListComponent } from './ofertas/ofertas-list.component';
+//import { OfertasListComponent } from './ofertas/ofertas-list.component';
 import { CategoriasListComponent } from './categorias/categorias-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'ofertas', pathMatch: 'full' },
-  { path: 'ofertas', component: OfertasListComponent },
+  //{ path: 'ofertas', component: OfertasListComponent },
   { path: 'categorias', component: CategoriasListComponent },
   { path: '**', redirectTo: 'ofertas' }
 ];
